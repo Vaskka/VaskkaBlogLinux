92157e6e5a93e059d8baa5cdbc32a0cac11a0be7
Vaskka <32007921+Vaskka@users.noreply.github.com>
2018-03-10 22:51:09 +0800

    添加创建文章权限

--- common/config/params.php
+++ common/config/params.php
@@ -5,4 +5,5 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'upload_url' => 'http://www.vaskka.com/',
     'default_label_img' => '/image/default/default_label_img.jpg',
+    'adminUserName' => 'Vaskka',
 ];
